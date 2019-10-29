57021b578179d1ec3ce34ec87c3ed3a803e3f99a
Adam Campbell <adam.92.ac@gmail.com>
2019-10-29 18:34:39 +1100

    Render weird gradient and animate it

--- code/macos_handmade.mm
+++ code/macos_handmade.mm
@@ -1,5 +1,57 @@
 #import <Cocoa/Cocoa.h>
 
+#define internal static
+#define local_persist static
+#define global_variable static
+
+struct macos_offscreen_buffer {
+    CGContextRef context;
+    void *memory;
+    int width;
+    int height;
+    int pitch; // This is bytes per row
+    int bytesPerPixel;
+};
+
+// Global for now
+global_variable macos_offscreen_buffer globalBackBuffer;
+global_variable CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
+
+internal void MacOsResizeBitmapContext(macos_offscreen_buffer *buffer, int width, int height) {
+    if (buffer->memory) {
+        free(buffer->memory);
+        CGContextRelease(buffer->context);
+    }
+
+    buffer->width = width;
+    buffer->height = height;
+    buffer->bytesPerPixel = 4;
+    buffer->pitch = width * buffer->bytesPerPixel;
+    buffer->memory = calloc(height, buffer->pitch);
+    buffer->context = CGBitmapContextCreate(buffer->memory, width, height, 8,
+                                            buffer->pitch, colorSpace, kCGImageAlphaNoneSkipLast);
+}
+
+internal void RenderWeirdGradient(macos_offscreen_buffer buffer, int xOffset, int yOffset) {
+    uint8_t *row = (uint8_t *)buffer.memory;
+    for (int y = 0; y < buffer.height; ++y) {
+        uint32_t *pixel = (uint32_t *)row;
+        for(int x = 0; x < buffer.width; ++x) {
+            uint8_t blue = x + xOffset;
+            uint8_t green = y + yOffset;
+
+            *pixel++ = ((blue << 16) | (green << 8));
+        }
+        row += buffer.pitch;
+    }
+}
+
+internal void MacOsDisplayBufferInWindow(macos_offscreen_buffer buffer, NSWindow *window) {
+    CGImageRef image = CGBitmapContextCreateImage(buffer.context);
+    window.contentView.layer.contents = (__bridge id)image;
+    CFRelease(image);
+}
+
 int main (int argc, char const *argv[]) {
     @autoreleasepool {
         NSString *appName = @"Handmade Hero";
@@ -33,8 +85,8 @@ int main (int argc, char const *argv[]) {
         [appMenu addItem:quitItem];
         [NSApp setMainMenu:menuBar];
 
-        // Setup window
-        NSRect frame = NSMakeRect(0, 0, 300, 300);
+        // Setup window and view
+        NSRect frame = NSMakeRect(0, 0, 1280, 720);
         NSWindowStyleMask styleMask = NSWindowStyleMaskTitled |
                                       NSWindowStyleMaskClosable |
                                       NSWindowStyleMaskResizable |
@@ -47,7 +99,11 @@ int main (int argc, char const *argv[]) {
         [window center];
         [window makeKeyAndOrderFront:nil];
 
+        MacOsResizeBitmapContext(&globalBackBuffer, 1280, 720);
+
         // Event loop
+        int xOffset = 0;
+
         while (true) {
             NSEvent *event;
 
@@ -58,6 +114,11 @@ int main (int argc, char const *argv[]) {
                                              dequeue:YES];
                 [NSApp sendEvent:event];
             } while (event != nil);
+
+            RenderWeirdGradient(globalBackBuffer, xOffset, 0);
+            MacOsDisplayBufferInWindow(globalBackBuffer, window);
+
+            xOffset++;
         }
     }
 }
